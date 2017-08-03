95a002abf3964ac05bb87d7706cb49d89140bdb4
Geoff GoeresHill <geoff.goeres@gmail.com>
2017-08-02 20:14:35 -0700

    Added instructions with input controller for keyboard selection

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -12,12 +12,29 @@ public class NumberWizard : MonoBehaviour {
 
 
 		print ("Welcome to Number Wizard");
-		print ("Pick a number between " + min +" and " + max +", but don't tell me!");
+		print ("Pick a number between " + min + " and " + max + ", but don't tell me!");
 
+		print ("Is the number higher or lower than 500?");
+		print ("Up = higher, down = lower, return = equal");
 	}
+
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Debug.Log ("Up arrow pressed");
+			print ("Number is higher than 500.");
+		} 
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Debug.Log ("Down arrow pressed");
+			print ("Number is lower than 500.");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			Debug.Log ("Return key pressed");
+			print ("Your number is 500!!");
+		}
 	}
 }
